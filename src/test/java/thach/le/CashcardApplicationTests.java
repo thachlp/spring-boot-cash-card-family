ef57b00bfb5cc0a981a05e0bc6cb06bb66b833f8
Thach Le <thach.le@linecorp.com>
2023-06-23 16:57:43 +0700

    Init project

---
+++ src/test/java/thach/le/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package thach.le;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
